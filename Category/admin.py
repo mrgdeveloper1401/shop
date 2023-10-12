7f94a6eeca402634cc9b24b403209b87109f6c99
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-12 12:18:15 +0330

    write model category and sub category in app Category

--- Category/admin.py
+++ Category/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CategoryModel, SubCategoryModel
 
-# Register your models here.
+
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('create_at','update_at', 'name')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(SubCategoryModel)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','slug')
+    list_filter = ('create_at','update_at', 'name')
+    prepopulated_fields = {'slug': ('name',)}
--- Category/models.py
+++ Category/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(_('Name'), max_length=50)
+    slug = models.SlugField(_('Slug'), unique=True)
+    create_at = models.DateTimeField(_('create at'), auto_now_add=True)
+    update_at = models.DateTimeField(_('update at'), auto_now=True)
+    
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categoryies')
+        db_table = 'category'
+        
+        
+class SubCategoryModel(models.Model):
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+    name = models.CharField(_('Name'), max_length=100)
+    slug = models.SlugField(_('Slug'), unique=True)
+    create_at = models.DateTimeField(_('create at'), auto_now_add=True)
+    update_at = models.DateTimeField(_('update at'), auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+    class Meta:
+        verbose_name = _('SubCategory')
+        verbose_name_plural = _('SubCategoryies')
+        db_table = 'sub_categories'
+        
